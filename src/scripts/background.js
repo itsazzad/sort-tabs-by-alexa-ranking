a8564d6c8d7df7930bf4d9cfa0101aabd6a7a41f
Sazzad Hossain Khan <itsazzad@gmail.com>
2017-07-02 02:43:20 +0600

    Basic Workflow added

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -10,3 +10,38 @@ ext.runtime.onMessage.addListener(
     }
   }
 );
+
+storage.set({ sites: value }, function() {
+});
+
+storage.get(site, function(resp) {
+  console.log(resp);
+  var site = resp[site];
+  if(site) {
+  } else {
+  }
+});
+
+ext.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  console.log(tabs);
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  alert('Tabs will be sorted');
+  chrome.windows.getCurrent(function(window)
+  {
+    var tabIds = [];
+    window.tabs.forEach(function(tab){
+      //get alexa info if the latest info not available
+
+      console.log(tab);
+      tabIds.push(tab.id);
+    });
+
+    //Sort the tabs here by id
+
+    chrome.tabs.move(tabIds, function(tabs){
+
+    });
+  });
+});
